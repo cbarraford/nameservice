072205a535d653fef3512a2f6df18e0d6b833393
Chad Barraford <cbarraford@gmail.com>
2019-06-14 12:37:08 +0200

    updates

--- cmd/nscli/main.go
+++ cmd/nscli/main.go
@@ -15,19 +15,9 @@ import (
 	"github.com/tendermint/tendermint/libs/cli"
 
 	app "github.com/cbarraford/nameservice"
-	nsclient "github.com/cbarraford/nameservice/x/nameservice/client"
-	nsrest "github.com/cbarraford/nameservice/x/nameservice/client/rest"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
-	auth "github.com/cosmos/cosmos-sdk/x/auth/client/rest"
 	bankcmd "github.com/cosmos/cosmos-sdk/x/bank/client/cli"
-	bank "github.com/cosmos/cosmos-sdk/x/bank/client/rest"
-	distcmd "github.com/cosmos/cosmos-sdk/x/distribution"
-	distClient "github.com/cosmos/cosmos-sdk/x/distribution/client"
-	dist "github.com/cosmos/cosmos-sdk/x/distribution/client/rest"
-	st "github.com/cosmos/cosmos-sdk/x/staking"
-	stakingclient "github.com/cosmos/cosmos-sdk/x/staking/client"
-	staking "github.com/cosmos/cosmos-sdk/x/staking/client/rest"
 )
 
 const (
@@ -47,12 +37,6 @@ func main() {
 	config.SetBech32PrefixForConsensusNode(sdk.Bech32PrefixConsAddr, sdk.Bech32PrefixConsPub)
 	config.Seal()
 
-	mc := []sdk.ModuleClient{
-		nsclient.NewModuleClient(storeNS, cdc),
-		stakingclient.NewModuleClient(st.StoreKey, cdc),
-		distClient.NewModuleClient(distcmd.StoreKey, cdc),
-	}
-
 	rootCmd := &cobra.Command{
 		Use:   "nscli",
 		Short: "nameservice Client",
@@ -68,8 +52,8 @@ func main() {
 	rootCmd.AddCommand(
 		rpc.StatusCommand(),
 		client.ConfigCmd(app.DefaultCLIHome),
-		queryCmd(cdc, mc),
-		txCmd(cdc, mc),
+		queryCmd(cdc),
+		txCmd(cdc),
 		client.LineBreak,
 		lcd.ServeCommand(cdc, registerRoutes),
 		client.LineBreak,
@@ -85,17 +69,11 @@ func main() {
 }
 
 func registerRoutes(rs *lcd.RestServer) {
-	rs.CliCtx = rs.CliCtx.WithAccountDecoder(rs.Cdc)
-	rpc.RegisterRPCRoutes(rs.CliCtx, rs.Mux)
-	tx.RegisterTxRoutes(rs.CliCtx, rs.Mux, rs.Cdc)
-	auth.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, storeAcc)
-	bank.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, rs.KeyBase)
-	nsrest.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, storeNS)
-	staking.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, rs.KeyBase)
-	dist.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc, distcmd.StoreKey)
+	client.RegisterRoutes(rs.CliCtx, rs.Mux, rs.Cdc)
+	app.ModuleBasics.RegisterRESTRoutes(rs.CliCtx, rs.Mux, rs.Cdc)
 }
 
-func queryCmd(cdc *amino.Codec, mc []sdk.ModuleClient) *cobra.Command {
+func queryCmd(cdc *amino.Codec) *cobra.Command {
 	queryCmd := &cobra.Command{
 		Use:     "query",
 		Aliases: []string{"q"},
@@ -103,22 +81,21 @@ func queryCmd(cdc *amino.Codec, mc []sdk.ModuleClient) *cobra.Command {
 	}
 
 	queryCmd.AddCommand(
+		authcmd.GetAccountCmd(cdc),
+		client.LineBreak,
 		rpc.ValidatorCommand(cdc),
 		rpc.BlockCommand(),
 		tx.SearchTxCmd(cdc),
 		tx.QueryTxCmd(cdc),
 		client.LineBreak,
-		authcmd.GetAccountCmd(storeAcc, cdc),
 	)
 
-	for _, m := range mc {
-		queryCmd.AddCommand(m.GetQueryCmd())
-	}
+	app.ModuleBasics.AddQueryCommands(queryCmd, cdc)
 
 	return queryCmd
 }
 
-func txCmd(cdc *amino.Codec, mc []sdk.ModuleClient) *cobra.Command {
+func txCmd(cdc *amino.Codec) *cobra.Command {
 	txCmd := &cobra.Command{
 		Use:   "tx",
 		Short: "Transactions subcommands",
@@ -130,11 +107,11 @@ func txCmd(cdc *amino.Codec, mc []sdk.ModuleClient) *cobra.Command {
 		authcmd.GetSignCommand(cdc),
 		tx.GetBroadcastCommand(cdc),
 		client.LineBreak,
+		tx.GetBroadcastCommand(cdc),
+		tx.GetEncodeCommand(cdc),
 	)
 
-	for _, m := range mc {
-		txCmd.AddCommand(m.GetTxCmd())
-	}
+	app.ModuleBasics.AddTxCommands(txCmd, cdc)
 
 	return txCmd
 }
